a6e94d8722b0330e6a5102302d1f4efe67377193
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-11-26 11:40:56 -0300

    Create users table

---
+++ database/migrations/2021_11_26_124307_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('type')->nullable(false)->default(1);
+            $table->string('name')->nullable(false);
+            $table->string('document_id')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
