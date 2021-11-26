efd44209975bebe3aaaf118b625e53ddb8646552
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-11-26 11:41:14 -0300

    Create transactions table

---
+++ database/migrations/2021_11_26_124417_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('payer')->nullable(false);
+            $table->integer('payee')->nullable(false);
+            $table->decimal('value')->nullable(false);
+            $table->timestamps();
+
+            $table->foreign('payer')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('payee')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
