3fac459f8ae4e3642b997a486e60fca75bdd1d9c
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-11-28 22:48:03 -0300

    Create controller for transactions

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,25 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Transaction;
-use App\Models\User;
+use App\Services\TransactionService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+use Laravel\Lumen\Routing\Controller as BaseController;
 
-
-class TransactionController extends Controller
+class TransactionController extends BaseController
 {
-    public function transactions()
+    protected TransactionService $service;
+
+    public function __construct(TransactionService $service)
     {
-        return Transaction::all();
+        $this->service = $service;
     }
 
     public function transaction(Request $request)
     {
-        return $request->toArray();
-    }
+        $payer_id = $request->get('payer');
+        $payee_id = $request->get('payee');
+        $value = $request->get('value');
 
-    public function users()
-    {
-        return User::all();
+        try {
+            DB::begintransaction();
+            $this->service->execute([$payer_id, $payee_id, $value]);
+            DB::commit();
+        } catch (ValidationException $ex) {
+            DB::rollBack();
+            return response()->json(['error' => $ex->validator->errors()->first()], 400);
+        }
+
+        return response()->json(['message' => 'Success']);
     }
 }
