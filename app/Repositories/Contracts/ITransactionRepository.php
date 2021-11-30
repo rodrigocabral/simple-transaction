3bd0a874a031b521c6072669dd1a886a788c15ef
Rodrigo Cabral <rbatista.ti@gmail.com>
2021-11-30 00:00:53 -0300

    Define new methods

--- app/Repositories/Contracts/ITransactionRepository.php
+++ app/Repositories/Contracts/ITransactionRepository.php
@@ -4,5 +4,11 @@ namespace App\Repositories\Contracts;
 
 interface ITransactionRepository
 {
-    public function save(array $attributes): bool;
+    public function save(array $attributes): object;
+
+    public function markAsNotified(int $transaction_id): bool;
+
+    public function markAsAuthorized(int $transaction_id): bool;
+
+    public function markAsConfirmed(int $transaction_id): void;
 }
